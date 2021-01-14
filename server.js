f0b0245f27080b59e8ed3f7c204a7471b80db18b
Your Name <almashossain121@gmail.com>
2021-01-14 10:12:51 -0800

    com

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express()
 const path = require('path')
 
-app.use(express.static(__dirname + '/dist'))
+app.use(express.static(__dirname + '/dist/normal'))
 app.listen(process.env.PORT || 8000);
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'))
+    res.sendFile(path.join(__dirname + '/dist/normal/index.html'))
 })
 
 console.log('Console listening');
